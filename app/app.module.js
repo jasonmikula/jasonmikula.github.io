312983ce1c2013e3ec006f51ec366589d5691814
Nick Wichert <nick.wichert@icloud.com>
2016-07-19 20:51:44 -0500

    initial commit

---
+++ app/app.module.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+    angular
+      .module('app', [
+      //Angular modules.
+      'ngRoute',
+
+      //Third Pary modules.
+      'firebase',
+
+      //Custom modules.
+      'app.landing'
+    ]);
+  
+})();
